d0052e6e62585fa234054a24dffc5cc4b2a2f365
mahmoudibrahimabdulfattah <mah.khalil22@gmail.com>
2024-08-26 22:19:36 +0300

    constant

--- app/src/main/java/com/mif/moviecatalogapp/utils/Constant.kt
+++ app/src/main/java/com/mif/moviecatalogapp/utils/Constant.kt
@@ -4,5 +4,4 @@ object Constant {
     const val BASE_URL = "https://api.themoviedb.org/3/movie/"
     const val API_KEY = "09a8fb2e4ec9e3f81d7d28d65f262cc7"
     const val baseImageUrl = "https://image.tmdb.org/t/p/w200"
-    const val API_Read_Access_Token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOWE4ZmIyZTRlYzllM2Y4MWQ3ZDI4ZDY1ZjI2MmNjNyIsIm5iZiI6MTcyNDY3MzUwOS42MzY3ODYsInN1YiI6IjY2Y2M2ZDIzMjdjMTg0YWRhNzE2OTBkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dNYKTsRaNM-101wRKRMbeHJfRAvjwo1abGPuR2yJkDk"
 }
